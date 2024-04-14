2b05e765cf5a8807d45a7a89a05ef264ee25ebb3
Mandariini <timo.moilanen99@outlook.com>
2024-04-14 14:47:08 +0300

    33. Broadcasting Initial Values

--- Source/Aura/UI/HUD/AuraHUD.cpp
+++ Source/Aura/UI/HUD/AuraHUD.cpp
@@ -26,7 +26,9 @@ void AAuraHUD::InitOverlay(APlayerController *PC, APlayerState *PS, UAbilitySyst
 
   FWidgetControllerParams WCP(PC, PS, ASC, AS);
   UOverlayWidgetController *WidgetController = GetOverlayWidgetController(WCP);
+  
   OverlayWidget->SetWidgetController(WidgetController);
+  WidgetController->BroadcastInitialValues();
 
   Widget->AddToViewport();
 }
--- Source/Aura/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/UI/WidgetController/OverlayWidgetController.cpp
@@ -2,3 +2,13 @@
 
 
 #include "OverlayWidgetController.h"
+
+#include "Aura/AbilitySystem/AuraAttributeSet.h"
+
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+  const auto AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+  
+  OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
+  OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+}
