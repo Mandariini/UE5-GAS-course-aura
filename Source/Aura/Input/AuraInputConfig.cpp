44b4a053935d7ab01b54b80f61b72472a0a69876
Mandariini <timo.moilanen99@outlook.com>
2024-05-09 12:45:33 +0300

    99. Input Config Data Asset

---
+++ Source/Aura/Input/AuraInputConfig.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag,
+                                                                   bool bLogNotFound) const
+{
+  for (const auto& InputAction : AbilityInputActions)
+  {
+    if (InputAction.InputTag == InputTag)
+    {
+      return InputAction.InputAction;
+    }
+  }
+
+  if (bLogNotFound)
+  {
+    UE_LOG(LogTemp, Error, TEXT("Can't find AbilityInputAction for InputTag [%s], on InputConfig [%s]"),
+           *InputTag.ToString(), *GetNameSafe(this));
+  }
+
+  return nullptr;
+}
