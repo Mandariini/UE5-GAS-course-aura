22c68971b3a6f97660d708cb8c4a1dd01ab88fb3
Mandariini <timo.moilanen99@outlook.com>
2024-05-15 20:55:12 +0300

    121. Projectile Impact

--- Source/Aura/Actor/AuraProjectile.cpp
+++ Source/Aura/Actor/AuraProjectile.cpp
@@ -3,8 +3,11 @@
 
 #include "AuraProjectile.h"
 
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 AAuraProjectile::AAuraProjectile()
 {
@@ -30,9 +33,39 @@ void AAuraProjectile::BeginPlay()
 {
   Super::BeginPlay();
 
+  SetLifeSpan(LifeSpan);
+
   Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+
+  LoopingSoundComponent = UGameplayStatics::SpawnSoundAttached(LoopingSound, GetRootComponent());
+}
+
+void AAuraProjectile::Destroyed()
+{
+  if (!bHit && !HasAuthority())
+  {
+    UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+    UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+    LoopingSoundComponent->Stop();
+  }
+
+  Super::Destroyed();
 }
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                       UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-                                      const FHitResult& SweepResult) {}
+                                      const FHitResult& SweepResult)
+{
+  UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+  UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+  LoopingSoundComponent->Stop();
+
+  if (HasAuthority())
+  {
+    Destroy();
+  }
+  else
+  {
+    bHit = true;
+  }
+}
