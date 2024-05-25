dcd0911f9990e0c77a7cb031e0c615747c2e42db
Mandariini <timo.moilanen99@outlook.com>
2024-05-25 12:08:36 +0300

    138. Dissolve Effect

--- Source/Aura/Character/AuraCharacterBase.cpp
+++ Source/Aura/Character/AuraCharacterBase.cpp
@@ -53,6 +53,8 @@ void AAuraCharacterBase::MulticastHandleDeath_Implementation()
   GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block); // Walls, floors etc.
 
   GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+  Dissolve();
 }
 
 void AAuraCharacterBase::BeginPlay()
@@ -99,4 +101,23 @@ void AAuraCharacterBase::AddCharacterAbilities() const
 
   UAuraAbilitySystemComponent* AuraASC = CastChecked<UAuraAbilitySystemComponent>(AbilitySystemComponent);
   AuraASC->AddCharacterAbilities(StartupAbilities);
+}
+
+void AAuraCharacterBase::Dissolve()
+{
+  if (IsValid(DissolveMaterialInstance))
+  {
+    UMaterialInstanceDynamic* DynamicMaterialInstance =
+      UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+    GetMesh()->SetMaterial(0, DynamicMaterialInstance);
+    StartDissolveTimeline(DynamicMaterialInstance);
+  }
+
+  if (IsValid(WeaponDissolveMaterialInstance))
+  {
+    UMaterialInstanceDynamic* DynamicMaterialInstance =
+      UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+    Weapon->SetMaterial(0, DynamicMaterialInstance);
+    StartWeaponDissolveTimeline(DynamicMaterialInstance);
+  }
 }
